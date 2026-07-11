2dfa6e6ffc55852ede6e7d4f4d977be5c78ae769
Subham777-max <subhamray865@gmail.com>
2026-07-11 14:30:34 +0530

    feat: update routing to include Dashboard, Project, and Import detail pages with protected routes

--- frontend/src/app/App.routes.jsx
+++ frontend/src/app/App.routes.jsx
@@ -2,18 +2,41 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from '../features/auth/pages/LoginPage'
 import RegisterPage from '../features/auth/pages/RegisterPage'
 import ProtectedRoute from '../utils/ProtectedRoutes'
+import DashboardPage from '../features/crm/pages/DashboardPage'
+import ProjectPage from '../features/crm/pages/ProjectPage'
+import ImportDetailPage from '../features/crm/pages/ImportDetailPage'
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <ProtectedRoute><h1>hello world</h1></ProtectedRoute>
+        element: (
+            <ProtectedRoute>
+                <DashboardPage />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/project/:projectId',
+        element: (
+            <ProtectedRoute>
+                <ProjectPage />
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/import/:importId',
+        element: (
+            <ProtectedRoute>
+                <ImportDetailPage />
+            </ProtectedRoute>
+        ),
     },
     {
         path: '/login',
-        element: <LoginPage />
+        element: <LoginPage />,
     },
     {
         path: '/register',
-        element: <RegisterPage />
+        element: <RegisterPage />,
     },
 ])
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import './app/index.css'
 import App from './app/App.jsx'
 import { AuthProvider } from './features/auth/Auth.context.provider.jsx'
 import { ToastProvider } from './global/context/ToastContext.jsx'
+import { CRMProvider } from './features/crm/CRM.context.provider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastProvider>
       <AuthProvider>
-        <App />
+        <CRMProvider>
+          <App />
+        </CRMProvider>
       </AuthProvider>
     </ToastProvider>
   </StrictMode>,
