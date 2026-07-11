5f379fd9d09a90e7b9a88eab58c540e8aa29ede7
Subham777-max <subhamray865@gmail.com>
2026-07-11 19:26:52 +0530

    feat: update API base URL to include '/api' for correct endpoint access

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://intelliimport.onrender.com",
+  baseURL: "https://intelliimport.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
